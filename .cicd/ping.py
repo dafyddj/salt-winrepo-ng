ed41b7c6ab4333bdd5b4c2cf0bf16f45c3ab2e24
Dafydd Jones <dafydd@techneg.it>
2025-03-19 17:02:42 +0000

    test: ping add logging

--- .cicd/ping.py
+++ .cicd/ping.py
@@ -1,20 +1,33 @@
-import salt.config
-import salt.loader
+import logging
+from salt._logging import *
+import sys
 
-overrides = {"root_dir": "/tmp/salt"} 
+setup_console_handler(LOG_LEVELS["trace"])
+
+# Configure the root logger to output to the console.
+#logging.basicConfig(
+#    level=salt._logging.TRACE,  # Set the desired level (DEBUG, INFO, etc.)
+#    stream=sys.stdout,    # Explicitly log to stdout; by default it goes to stderr.
+#    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+#)
+
+import salt.config
+import salt.client
+
+logging.info("Salt API logging initialized.")
+
+overrides = {"file_client": "local", "log_level_logfile": "info", "providers": {"winrepo": "winrepo"},  "root_dir": "/tmp/salt"} 
 __opts__ = salt.config.apply_minion_config(overrides | {"grains": {"cpuarch": "AMD64"}})
 
-__utils__ = salt.loader.utils(__opts__)
-__salt__ = salt.loader.minion_mods(__opts__)
+caller = salt.client.Caller(mopts=__opts__)
 
-testmod = salt.loader.raw_mod(__opts__, 'test', None)
-print(testmod['test.ping']())
-print(testmod['test.get_opts']())
+print(caller.cmd("test.ping"))
 
-grainsmod = salt.loader.raw_mod(__opts__, "grains", None)
-print(grainsmod["grains.items"]())
 
-winrepomod = salt.loader.raw_mod(__opts__, 'winrepo', __salt__)
-print(winrepomod["winrepo.show_sls"]("arduino-ide.sls"))
-print(winrepomod["winrepo.show_sls"]("vscode.sls"))
-print(winrepomod["winrepo.show_sls"]("openvpn.sls"))
+print(caller.cmd("test.get_opts"))
+
+print(caller.cmd("grains.items"))
+
+print(caller.cmd("winrepo.show_sls", "arduino-ide.sls"))
+print(caller.cmd("winrepo.show_sls", "vscode.sls"))
+print(caller.cmd("winrepo.show_sls", "openvpn.sls"))
